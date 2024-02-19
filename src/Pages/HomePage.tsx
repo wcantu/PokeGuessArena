239b5ab2a45300a700a3545595f55b4015dee267
wcantu <45667892+wcantu@users.noreply.github.com>
2024-02-19 02:06:10 -0500

    added fandom-py api library, set up shell page, css for button

---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,15 @@
+// HomePage.tsx
+
+import React from 'react';
+
+// Define your component
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      {/* Your component JSX here */}
+    </div>
+  );
+};
+
+// Export the component
+export default HomePage;
---
+++ src/pokemon_info.py
@@ -0,0 +1,8 @@
+from fandom import Fandom
+
+
+
+
+
+
+
